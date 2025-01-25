6912807d182f94434f171d492060bb7f0933eea6
kfh83 <85626296+kfh83@users.noreply.github.com>
2025-01-25 17:06:26 -0300

    farming commits atp

--- initcab.cpp
+++ initcab.cpp
@@ -1985,7 +1985,7 @@ int ExplorerWinMain(HINSTANCE hInstance, HINSTANCE hPrev, LPTSTR pszCmdLine, int
             if (!IsAnyShellWindowAlreadyPresent())
             {
                 //temp
-                SetThemeAppProperties(NULL);
+                //SetThemeAppProperties(NULL);
                 hDesktop = CreateDesktopAndTray();
             }
 
